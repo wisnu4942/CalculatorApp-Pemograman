d78431325e857ced7dc2d91f793d75b9a2a9f523
wisnu4942 <wisnu_nugroho@students.amikom.ac.id>
2023-03-13 16:57:55 +0700

    menambahkan perkalian dan pembagian

--- Calculator.cs
+++ Calculator.cs
@@ -7,6 +7,8 @@ class Calculator
 		
 		Console.WriteLine("Hasil Penambahan: (0) + (1) = (2)", a, b, Penambahan(a,b));
 		Console.WriteLine("Hasil Pengurangan: (0) - (1) = (2)", a, b, Pengurangan(a,b)); 
+		Console.WriteLine("Hasil Perkalian: (0) * (1) = (2)", a, b, Perkalian(a,b));
+		Console.WriteLine("Hasil Pembagianan: (0) / (1) = (2)", a, b, Pembagianan(a,b));
 		
 		Console.WriteLine("\nTekan sembarang untuk keluar");
 		Console.ReadKey();
@@ -20,5 +22,15 @@ class Calculator
 	static void Pengurangan(int a, int b)
 	{
 		return a - b;
+	}
+	
+	static void Perkalian(int a, int b)
+	{
+		return a * b;
+	}
+	
+	static void Pembagianan(int a, int b)
+	{
+		return a / b;
 	}
 }	
